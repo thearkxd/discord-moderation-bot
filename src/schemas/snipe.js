57b15a14e8e072ec09ce0bd8eb09538794a557e4
Theark <59927799+thearkxd@users.noreply.github.com>
2021-07-02 19:47:23 +0300

    Update snipe.js

--- src/schemas/snipe.js
+++ src/schemas/snipe.js
@@ -3,7 +3,7 @@ const { Schema, model } = require("mongoose");
 const schema = Schema({
   guildID: { type: String, default: "" },
   channelID: { type: String, default: "" },
-  userID: { type: String, default: "" },
+  author: { type: String, default: "" },
   messageContent: { type: String, default: "" },
   image: { type: String, default: "" },
   createdDate: { type: Number, default: Date.now() },
